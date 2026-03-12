00a1f9af3bced0a625192ad6efa19cd0e72023a9
EKL-NCI <x22353461@student.ncirl.ie>
2026-03-12 17:44:15 +0000

    Updated pipeline to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ sudo npm install -g pm2
 cd ~/CA1DevOpSecSimpleApplication
 
 npm install
+echo "$PRIVATE_KEY" > privatekey.pem
+echo "$SERVER" > server.crt
 
 pm2 delete SimpleApplication || true
 pm2 start ./bin/www --name SimpleApplication 
