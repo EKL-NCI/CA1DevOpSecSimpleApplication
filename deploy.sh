6274ced8a3f0ab65b6d1080081da4b593c6acc7b
EKL-NCI <x22353461@student.ncirl.ie>
2026-03-12 18:08:17 +0000

    added https www

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 sudo apt update
-sudo apt install nodejs npm
+sudo apt install -y nodejs npm
 
 sudo npm install -g pm2
 
-cd ~/CA1DevOpSecSimpleApplication
+cd ~/CA1DevOpSecSimpleApplication || exit 1
 
 npm install
-echo "$PRIVATE_KEY" > privatekey.pem
-echo "$SERVER" > server.crt
+
+printf "%s\n" "$PRIVATE_KEY" > privatekey.pem
+printf "%s\n" "$SERVER" > server.crt
+
+chmod 600 privatekey.pem server.crt
 
 pm2 delete SimpleApplication || true
-pm2 start ./bin/www --name SimpleApplication 
+pm2 start ./bin/www --name SimpleApplication --env=production
